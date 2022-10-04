4b278df3645192cb0fb21524f19e7665d6a04760
Dustin McGuire <dustin.mcguire@red-gate.com>
2022-10-04 13:54:19 -0500

    commit to repository

---
+++ Tables/dbo.Table1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table1]
+(
+[Id] [int] NULL,
+[Name] [varchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
---
+++ Tables/dbo.TestTable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[TestTable]
+(
+[aReallyLongNameToTest1] [int] NULL,
+[aReallyLongNameToTest2] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest3] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest4] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest5] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest6] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest7] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest8] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest9] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest10] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest11] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[aReallyLongNameToTest12] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
